8941b4057dcacebaf32089bae53551a4c3256403
peilisher <peilisher@gmail.com>
2020-07-29 18:33:58 +0800

    test

--- src/main/java/gld/Main.java
+++ src/main/java/gld/Main.java
@@ -7,21 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(n);
-        System.out.println(n);
-        asd
-                gsaedg
-        System.out.println(n);
-        gqawkejr;
-        System.out.println(n);
-        odfg;ll;s
-        System.out.println(n);
-        System.out.println(n);
-        System.out.println(n);
-        System.out.println(n);
-        System.out.println(n);
-        System.out.println(n);
-        System.out.println(n);
-        System.out.println(n);
+afggfadg
     }
 }
